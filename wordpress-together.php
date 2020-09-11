0400a47cc0c65b0cc5aee71cd7686b1715561604
Jimmy Bisenius <me@jimmybweb.com>
2020-09-11 18:12:13 -0400

    Adding WordPress plugin assets

---
+++ wordpress-together.php
@@ -0,0 +1,16 @@
+<?php
+/*
+* Plugin Name: WordPress Together
+* Version: 0.1.0
+* Plugin URI: https://github.com/Neutron-Creative/WordPress-Together
+* Description: Easily integrate Mozilla's TogetherJS collaboration system into a Wordpress site.
+* Author: Neutron Creative Inc.
+* Author URI: https://github.com/Neutron-Creative
+* Requires at least: 4.0.0
+* Tested up to: 5.5.0
+*
+* @package WordPress
+* @author Neutron Creative Inc.
+* @since 1.0.0
+*/
+?>
